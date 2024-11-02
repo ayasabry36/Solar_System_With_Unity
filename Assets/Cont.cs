43966bd62d10ca944e757280c30009f8f8546e20
ayasabry36 <ayaasaprey123@gmail.com>
2024-11-01 17:56:51 -0700

    Educational Solar System
    Interactive Educational Solar System for kids that contains all the planets with their names and sounds

---
+++ Assets/Cont.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cont : MonoBehaviour {
+  
+    public Text planetNameText;
+    public AudioSource audioSource;
+    public AudioClip mercuryClip;
+    public AudioClip venusClip;
+    public AudioClip earthClip;
+    public AudioClip marsClip;
+    public AudioClip jupiterClip;
+    public AudioClip saturnClip;
+    public AudioClip uranusClip;
+    public AudioClip neptuneClip;
+    public AudioClip plutoClip;
+
+    public void ShowMercury()
+    {
+        planetNameText.text = "Mercury";
+        PlayMusic(mercuryClip);
+    }
+
+    public void ShowVenus()
+    {
+        planetNameText.text = "Venus";
+        PlayMusic(venusClip);
+    }
+
+    public void ShowEarth()
+    {
+        planetNameText.text = "Earth";
+        PlayMusic(earthClip);
+    }
+
+    public void ShowMars()
+    {
+        planetNameText.text = "Mars";
+        PlayMusic(marsClip);
+    }
+
+    public void ShowJupiter()
+    {
+        planetNameText.text = "Jupiter";
+        PlayMusic(jupiterClip);
+    }
+
+    public void ShowSaturn()
+    {
+        planetNameText.text = "Saturn";
+        PlayMusic(saturnClip);
+    }
+
+    public void ShowUranus()
+    {
+        planetNameText.text = "Uranus";
+        PlayMusic(uranusClip);
+    }
+
+    public void ShowNeptune()
+    {
+        planetNameText.text = "Neptune";
+        PlayMusic(neptuneClip);
+    }
+    public void ShowPluto()
+    {
+        planetNameText.text = "Pluto";
+        PlayMusic(plutoClip);
+    }
+    private void PlayMusic(AudioClip clip)
+    {
+        if (audioSource.isPlaying)
+        {
+            audioSource.Stop();
+        }
+        audioSource.clip = clip;
+        audioSource.Play();
+    }
+
+
+}
